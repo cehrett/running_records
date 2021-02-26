7b8574cc321a7f9d8b33afd2b1021b026ee6be9c
Carl Ehrett <cehrett@gmail.com>
2021-02-26 11:47:46 -0500

    Just fixing typos

---
+++ AuthenticateGoogleDrive.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9493b3441f54c0fe95590e45fd3741eb974a41ca1b672c4e2ecf3fe075360fbf
+size 560
---
+++ google_drive_quickstart.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f2a7e324a232249ced453ddccb8975588ca4694f1ea1c748488c8e8e36dae9d3
+size 1764
