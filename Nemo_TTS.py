eef588cbfe0fa4d08a01ded2dec09183af5032db
DillonRanwala <68616281+DillonRanwala@users.noreply.github.com>
2021-04-16 13:50:44 -0400

    Update Nemo_TTS.py
    Converting all return audio types to a numpy array

--- Nemo_TTS.py
+++ Nemo_TTS.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:16f4dbe8393f2070de536e7eddc8cc1f95022c59ca2372fd7e4a716c4f8a1a89
-size 4137
+oid sha256:eb38361dcc6d62ad3b96bdeb5d3dd5b129d0f975773b4332ecfb20daafc5da42
+size 4083
