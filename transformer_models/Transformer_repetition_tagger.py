591fd6a9460a9edc24201f1f701a56cf64fe6c8c
Carl Ehrett <cehrett@gmail.com>
2021-07-30 14:16:16 -0400

    Add transformer model notebooks and scripts

---
+++ transformer_models/Transformer_repetition_tagger.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0d2f1175fe718f5f4e4a0a04c0cdaf8e9aad07a4783c4f4d4322d159e16e9348
+size 56012
---
+++ transformer_models/Transformer_repetition_tagger_with_truetext_decoding.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0bb7774afbed243cfc46222c41b84cc35ca030247b0a70a2e91cf8ae776f04a4
+size 57273
