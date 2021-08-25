c9b354b3718f221a39b272088e12abba5b03fa8e
Carl Ehrett <cehrett@gmail.com>
2021-08-25 15:26:03 -0400

    Added early stopping

--- transformer_models/transformer_repetition_kit.py
+++ transformer_models/transformer_repetition_kit.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e68b0641c30cd5b7545558de28baff66ec7f4f1b26d8c1260e150688fee51548
-size 38486
+oid sha256:aa8d0d8ae78685e25b5b2a1a4b55df076effe270ede3bc8851aa847564e6f073
+size 38986
