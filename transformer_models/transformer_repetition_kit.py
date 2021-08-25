3b3d9d60852bfe3541df9c4f47860aad93907dd0
Carl Ehrett <cehrett@gmail.com>
2021-08-25 15:29:20 -0400

    Fix early stopping

--- transformer_models/transformer_repetition_kit.py
+++ transformer_models/transformer_repetition_kit.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa8d0d8ae78685e25b5b2a1a4b55df076effe270ede3bc8851aa847564e6f073
-size 38986
+oid sha256:a1a867a95cbab719e3491900dd16cf305e4b1e471a6387490c7160d8c242acc8
+size 38987
