34424c5c24ac97d42054d7f984bc48d498dd0ee7
Carl Ehrett <cehrett@gmail.com>
2022-02-21 11:28:03 -0500

    Added attribution of code source

--- transformer_models/transformer_repetition_kit.py
+++ transformer_models/transformer_repetition_kit.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f39bf99fe37940f0f8d969f77b7c7c53bbbdde969e4e4adb155bb3ce04355a7
-size 39194
+oid sha256:60ccae8934d4bcdc841125da8eeee66573d9e4a8200bff89779821131099fc92
+size 39405
