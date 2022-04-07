670699d0fadefa3eaf14dfb98fac363da6702927
Jonathan Daniel <jmdanie@clemson.edu>
2022-04-07 13:18:12 -0400

    Added TODO re: 2-letter code

--- repetition_data_generation/create_text_training_data.py
+++ repetition_data_generation/create_text_training_data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:95e6b03f33ff7f88923ca1f2bd5af424b7674b8bde756e7985547d083083d3a8
-size 4910
+oid sha256:39e57951e6c18a2af92148694efaf0a71176751bd4e3c15a9b22f52709ba29e9
+size 4979
