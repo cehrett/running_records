e5da7d90d77d049c97e8995f6d5958bf664f11e6
Jonathan Daniel <jmdanie@clemson.edu>
2022-04-14 13:55:39 -0400

    Updated Training Scripts

--- repetition_data_generation/create_audio_training_data.py
+++ repetition_data_generation/create_audio_training_data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a2d30bdb3fbd999ed3b673d8157e929247006e8c9f7530abbfb52e84ba23c57
-size 1554
+oid sha256:3a24f6b50a27a99c4b4bb57267f5e17dbcc8cc5560e3d5b529abc1dd0c4890cb
+size 1564
--- repetition_data_generation/create_text_training_data.py
+++ repetition_data_generation/create_text_training_data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:40313fe83bd69608929b6092803b5bbfd8bb69447578df63dabe2acb5aa12149
-size 6217
+oid sha256:7ae6f0b033f483847b55d5159a98240e728134e5c3b4c48720bbc93c73d5c3eb
+size 6495
