a6f11e1906b31e5bf6a993eb8b7f1ca20cc19927
Jonathan Daniel <jmdanie@clemson.edu>
2022-04-14 15:00:05 -0400

    Update Logging

--- repetition_data_generation/create_audio_training_data.py
+++ repetition_data_generation/create_audio_training_data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a24f6b50a27a99c4b4bb57267f5e17dbcc8cc5560e3d5b529abc1dd0c4890cb
-size 1564
+oid sha256:3b10a73bcb2ae7174af42681b589b4f6fbcc4e45be1fc38c8a74143950fe59a1
+size 1598
