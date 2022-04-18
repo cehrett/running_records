ae0fc3b70db91e9ad94f37f7c2405c6ec411513f
Jonathan Daniel <jmdanie@clemson.edu>
2022-04-18 14:49:52 -0400

    Added some extra processing

--- repetition_data_generation/create_text_training_data.py
+++ repetition_data_generation/create_text_training_data.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5b2fc5b911c963e4a4b60f19becc30c10474a9a06df5728853df4ae3d5370ac8
-size 6728
+oid sha256:e9e9fe58e2c0b3b754154dd80d38aab775bdbc56a0c605f3710427b4e85ea58f
+size 6889
