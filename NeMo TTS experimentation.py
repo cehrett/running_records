32e4a5e3260c236fd164d96795ac538935f4e59d
Jonathan Daniel <gcagrad19@gmail.com>
2022-04-26 15:28:23 -0400

    Updated Root Files

--- NeMo TTS experimentation.py
+++ NeMo TTS experimentation.py
@@ -1,3 +1,109 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1ac4dd5da9e8f321cc9a783df85707017609944593790867d5d76c02e6ce559e
-size 4182
+
+import numpy as np
+from nemo.collections.tts.models.base import SpectrogramGenerator, Vocoder
+import random
+
+def convert_to_audio(sentence, spectrogram_generator='random', audio_generator='random'):
+  supported_spec_gen = ["tacotron2", "glow_tts","random"]
+  supported_audio_gen = ["waveglow", "squeezewave", "uniglow", "melgan", "hifigan", "two_stages","random"]
+
+  # randomly chooses model types if not specified in function call
+  if spectrogram_generator == 'random':
+    spectrogram_generator = random.choice([x for x in supported_spec_gen if x != "random"])
+
+  if audio_generator == 'random':
+    audio_generator = random.choice([x for x in supported_audio_gen if x != "random"])
+    
+  # choosing specified spectrogram model
+  override_conf = None
+  if spectrogram_generator == "tacotron2":
+      from nemo.collections.tts.models import Tacotron2Model
+
+      pretrained_model_spec = "tts_en_tacotron2"
+
+  elif spectrogram_generator == "glow_tts":
+      from nemo.collections.tts.models import GlowTTSModel
+
+      pretrained_model_spec = "tts_en_glowtts"
+      import wget
+      from pathlib import Path
+
+      if not Path("cmudict-0.7b").exists():
+          filename = wget.download("http://svn.code.sf.net/p/cmusphinx/code/trunk/cmudict/cmudict-0.7b")
+          filename = str(Path(filename).resolve())
+      else:
+          filename = str(Path("cmudict-0.7b").resolve())
+      conf = SpectrogramGenerator.from_pretrained(pretrained_model_spec, return_config=True)
+
+      if "params" in conf.parser:
+          conf.parser.params.cmu_dict_path = filename
+      else:
+          conf.parser.cmu_dict_path = filename
+      override_conf = conf
+  else:
+      raise NotImplementedError
+
+  # final spectrogram generator
+  spec_generator = SpectrogramGenerator.from_pretrained(pretrained_model_spec, override_config_path=override_conf)
+
+  # choosing specified vocoder (audio) model
+  RequestPseudoInverse = False
+  TwoStagesModel = False
+
+  if audio_generator == "waveglow":
+      from nemo.collections.tts.models import WaveGlowModel
+
+      pretrained_model_vocoder = "tts_waveglow_88m"
+  elif audio_generator == "squeezewave":
+      from nemo.collections.tts.models import SqueezeWaveModel
+
+      pretrained_model_vocoder = "tts_squeezewave"
+  elif audio_generator == "uniglow":
+      from nemo.collections.tts.models import UniGlowModel
+
+      pretrained_model_vocoder = "tts_uniglow"
+  elif audio_generator == "melgan":
+      from nemo.collections.tts.models import MelGanModel
+
+      pretrained_model_vocoder = "tts_melgan"
+  elif audio_generator == "hifigan":
+      from nemo.collections.tts.models import HifiGanModel
+
+      pretrained_model_vocoder = "tts_hifigan"
+
+  elif audio_generator == "two_stages":
+      from nemo.collections.tts.models import TwoStagesModel
+
+      cfg = {'linvocoder': {'_target_': 'nemo.collections.tts.models.two_stages.GriffinLimModel',
+                            'cfg': {'n_iters': 64, 'n_fft': 1024, 'l_hop': 256}},
+            'mel2spec': {'_target_': 'nemo.collections.tts.models.two_stages.MelPsuedoInverseModel',
+                          'cfg': {'sampling_rate': 22050, 'n_fft': 1024,
+                                  'mel_fmin': 0, 'mel_fmax': 8000, 'mel_freq': 80}}}
+      vocoder = TwoStagesModel(cfg)
+     
+      TwoStagesModel = True
+  else:
+      raise NotImplementedError
+  if not TwoStagesModel:
+    vocoder = Vocoder.from_pretrained(pretrained_model_vocoder)
+  # All spectrogram generators start by parsing raw strings to a tokenized version of the string
+  parsed = spec_generator.parse(sentence)
+
+  # They then take the tokenized string and produce a spectrogram
+  spectrogram = spec_generator.generate_spectrogram(tokens=parsed)
+  # Finally, a vocoder converts the spectrogram to audio
+  audio = vocoder.convert_spectrogram_to_audio(spec=spectrogram)
+  
+  # converting audio type to a numpy array if needed
+  try:
+    audio = audio.detach().numpy()
+  except:
+    print('Audio already is numpy array')
+  print('Spectrogram Generator: ' + spectrogram_generator)
+  print('Audio Generator: ' + audio_generator)
+
+  return audio
+
+if __name__ == '__main__':
+    audio = convert_to_audio('Testing testing testing')
+    print(audio)
--- Nemo ASR.py
+++ Nemo ASR.py
@@ -1,3 +1,161 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:19b18ba65e2f5eb8ec83044692b02edfe0a937f1de66adf98f5f2b83a83d9ca3
-size 3833
+# %%
+
+# NeMo's "core" package
+import sys
+
+print(sys.version)
+import nemo
+# NeMo's ASR collection - this collections contains complete ASR models and
+# building blocks (modules) for ASR
+import nemo.collections.asr as nemo_asr
+import json
+import librosa
+import os
+import pandas as pd
+from pydub import AudioSegment
+
+
+# %% md
+
+# Making JSON Manifest Files for Nemo Training
+
+# %%
+
+# --- Building Manifest Files --- #
+
+# Function to build a manifest
+def build_manifest_df(df, manifest_path, root_of_data='.\\data\\', Windows = True):
+    with open(manifest_path, 'w') as fout:
+        for index, row in df.iterrows():
+            if Windows:
+                row.filepath = '\\'.join(row.filepath.split('/'))
+                delimeter = '\\'
+            else:
+                delimeter = '/'
+            # mp3 to wav
+            mp3sound = AudioSegment.from_mp3(root_of_data + str(row.filepath).rsplit(delimeter, 1)[1])
+            wav_path = (root_of_data + (str(row.filepath)).strip('.mp3') + '.wav').rsplit(delimeter, 1)[1]
+            mp3sound.export(root_of_data + wav_path, format="wav")
+            duration = librosa.core.get_duration(filename=root_of_data + wav_path)
+            # Write the metadata to the manifest
+            metadata = {
+                "audio_filepath": root_of_data + wav_path,
+                "duration": duration,
+                "text": row['Cleaned audio transcript']
+            }
+            json.dump(metadata, fout)
+            fout.write('\n')
+
+
+# %%
+
+# Using audio data spreadsheet to build the manifests
+print(os.getcwd())
+df = pd.read_csv('.\\data\\running_records_audio_info.csv')
+
+# %%
+
+train_df = df[0:22].copy()
+valid_df = df[23:36].copy()
+
+# %%
+
+build_manifest_df(train_df, './data/train_manifest_all.json')
+build_manifest_df(valid_df, './data/test_manifest_all.json')
+# %% md
+
+# Out of Box Model Usage
+
+# %%
+
+# scr
+# This line will download pre-trained QuartzNet15x5 model from NVIDIA's NGC cloud and instantiate it for you
+quartznet = nemo_asr.models.EncDecCTCModel.from_pretrained(model_name="QuartzNet15x5Base-En")
+
+# %%
+
+# scr
+quartznet.transcribe(paths2audio_files=['.\\data\\ClarkSusan.wav'])
+
+# %%
+# Get intermediate output from Quartznet model
+
+activation = {}
+def get_activation(name):
+    def hook(model, input, output):
+        activation[name] = output.detach()
+    return hook
+
+
+model = quartznet
+model.decoder.decoder_layers.register_forward_hook(get_activation('decoder_layers'))
+x=['.\\data\\CollinsCole.wav']
+output = model.transcribe(paths2audio_files=x)
+print(activation['decoder_layers'])
+
+# %%
+
+# Listen to the audio file we're transcribing:
+from pydub.playback import play
+
+audiofile = '..\\data\\ClarkSusan.wav'
+AudioSegment.from_mp3(audiofile)
+
+# %% md
+
+# Training (in progress)
+
+# %%
+
+# Need to git clone NeMo only once. Don't include it in (our) git repository.
+# !git clone https://github.com/NVIDIA/NeMo/
+
+# %%
+
+# --- Config Information ---#
+try:
+    from ruamel.yaml import YAML
+except ModuleNotFoundError:
+    from ruamel_yaml import YAML
+# config_path = 'quartznet_15x5.yaml'
+
+yaml = YAML(typ='safe')
+with open('../NeMo/examples/asr/conf/quartznet_15x5.yaml') as f:
+    params = yaml.load(f)
+print(params)
+
+# %%
+
+import pytorch_lightning as pl
+
+# %%
+
+trainer = pl.Trainer(gpus=0, max_epochs=5)
+
+# %%
+
+train_mainfest = '../data/train_manifest_all.json'
+test_manifest = '../data/test_manifest_all.json'
+
+# %%
+
+params['model']
+
+# %%
+
+params['model']['train_ds']['max_duration'] = 500
+
+# %%
+
+from omegaconf import DictConfig
+
+params['model']['train_ds']['manifest_filepath'] = '../data/train_manifest_all.json'
+params['model']['validation_ds']['manifest_filepath'] = '../data/test_manifest_all.json'
+first_asr_model = nemo_asr.models.EncDecCTCModel(cfg=DictConfig(params['model']), trainer=trainer)
+
+# %%
+
+# Start training!!!
+trainer.fit(first_asr_model)
+
+# %%
